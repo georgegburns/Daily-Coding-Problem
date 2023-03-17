90b66f542e9f709c91e4263a8689c54b1ceb78a3
georgegburns <88833145+georgegburns@users.noreply.github.com>
2023-03-17 17:19:13 +0000

    Add files via upload

---
+++ Easy - 230317.py
@@ -0,0 +1,39 @@
+# This problem was asked by Alibaba.
+# Given an even number (greater than 2), return two prime numbers whose sum will be equal to the given number.
+# A solution will always exist. See Goldbach’s conjecture.
+# Example:
+# Input: 4
+# Output: 2 + 2 = 4
+# If there are more than one solution possible, return the lexicographically smaller solution.
+# If [a, b] is one solution with a <= b, and [c, d] is another solution with c <= d, then
+# [a, b] < [c, d]
+# If a < c OR a==c AND b < d.
+
+
+def SieveofEratosthenes(n : int):
+    # a list of possible primes
+    primes = list(range(2, n+1))
+    
+    # 2 is the first prime
+    i = 2
+    
+    # the sieve works by removing every number that is a multiple of a prime starting at the first prime 2
+    while i * i <= n:        
+        for j in range(i * i, n+1, i):
+            if (j in primes):
+                primes.remove(j)
+        i += 1
+    return primes
+
+
+def Goldbachsconjecture(n : int):
+    # using the sieve to generate the prime numbers up to the required result
+    lst = SieveofEratosthenes(n)
+    for i in lst: 
+        for j in reversed(lst):
+            # by working in both directions it garuantees the smallest first number + the smallest second number
+            if i + j == n:
+                result = (j,i)
+    return result
+            
+print(Goldbachsconjecture(300))
